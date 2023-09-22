25cdb43ba0b3651741b0ff4d3d90db0bb7395c86
Jonatas Freire <jonatas.freire@ab-inbev.com>
2023-09-22 14:44:27 -0300

    fix strings

--- SpreadSheetGenerator/main.go
+++ SpreadSheetGenerator/main.go
@@ -4,20 +4,29 @@ import (
 	"bytes"
 	"encoding/csv"
 	"encoding/json"
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 	"shared/config"
+	"shared/log"
 	"shared/utils"
 	"sort"
 	"strings"
+	"time"
 )
 
 func main() {
+	startTime := time.Now()
 	if len(config.File.Locales) == 0 || config.File.Locales == nil {
 		// return nil, errors.New("Theres is no Locale strings in config.json")
 		//errr
 	}
+
+	defer func() {
+		if msg := recover(); msg != nil {
+			log.WriteFile(fmt.Errorf("%v", msg).Error())
+		}
+	}()
 
 	// for _, localePath := range config.File.Locales {
 	// 	localeFolderPaths := make(map[string]string)
@@ -44,16 +53,24 @@ func main() {
 		nfaFileName, err := utils.GetNfaFileName(localePath)
 
 		if err != nil {
-			//return nil, err
+			log.WriteFile(err.Error())
 		}
 
-		localesFolders, _ := os.ReadDir(localePath)
+		localesFolders, err := os.ReadDir(localePath)
+
+		if err != nil {
+			log.WriteFile(err.Error())
+		}
 
 		for _, localeFolder := range localesFolders {
 			teste := make(map[string]string)
-			filesInLocaleFolder, _ := os.ReadDir(filepath.Join(localePath, localeFolder.Name()))
-			jsonFile, _ := os.Open(filepath.Join(localePath, localeFolder.Name(), filesInLocaleFolder[0].Name()))
+			filesInLocaleFolder, err := os.ReadDir(filepath.Join(localePath, localeFolder.Name()))
+			jsonFile, err := os.Open(filepath.Join(localePath, localeFolder.Name(), filesInLocaleFolder[0].Name()))
 			defer jsonFile.Close()
+
+			if err != nil {
+				log.WriteFile(err.Error())
+			}
 
 			var jsonData any
 			json.NewDecoder(jsonFile).Decode(&jsonData)
@@ -63,15 +80,19 @@ func main() {
 			localeFolderPaths[localeFolder.Name()] = teste
 		}
 
-		// jsonDataEncoded, _ := json.MarshalIndent(localeFolderPaths, "", "  ")
+		jsonDataEncoded, _ := json.MarshalIndent(localeFolderPaths, "", "  ")
 
-		// fmt.Println(string(jsonDataEncoded))
+		fmt.Println(string(jsonDataEncoded))
+
+		log.WriteFile(string(jsonDataEncoded))
+
+		time := time.Now().Format("02-01-2006_15:04")
 
 		// Cria CSV para cada repo
-		file, err := os.Create(nfaFileName + ".csv")
+		file, err := os.Create(nfaFileName + "_" + time + ".csv")
 
 		if err != nil {
-			log.Fatal("Error while reading the file", err)
+			log.WriteFile(err.Error())
 		}
 
 		defer file.Close()
@@ -94,18 +115,20 @@ func main() {
 
 		// Escreva os cabeçalhos de coluna no arquivo CSV
 		if err := writerCSV.Write(append([]string{""}, locales...)); err != nil {
-			log.Fatal(err)
+			log.WriteFile(err.Error())
 		}
 
 		// Percorra as subchaves (rótulos de linha) e escreva os valores no arquivo CSV
 		for subchave := range localeFolderPaths[locales[0]] {
 			linha := []string{subchave}
 			for _, chaveExterna := range locales {
-				linha = append(linha, localeFolderPaths[chaveExterna][subchave])
+				valor := localeFolderPaths[chaveExterna][subchave]
+				linha = append(linha, `"`+valor+`"`)
 			}
 			if err := writerCSV.Write(linha); err != nil {
-				log.Fatal(err)
+				log.WriteFile(err.Error())
 			}
+			log.WriteFile(subchave)
 		}
 
 		// Certifique-se de liberar os recursos e escrever os dados no arquivo
@@ -113,27 +136,23 @@ func main() {
 
 		// Verificar erros após o flush
 		if err := writerCSV.Error(); err != nil {
-			log.Fatal(err)
+			log.WriteFile(err.Error())
 		}
 
 		// Escreva o buffer no arquivo
 		file.Write(buffer.Bytes())
 
 		if err != nil {
-			log.Fatal("Error while reading the file", err)
+			log.WriteFile(err.Error())
 		}
 
 	}
 
-	// r := csv.NewWriter(file).Write([]string{"teste", "outroTeste"})
+	endTime := time.Now()
 
-	// // r, err := csv.NewReader(file).ReadAll()
-
-	// // if err != nil {
-	// // 	log.Fatal("Error while reading the file", err)
-	// // }
-
-	// fmt.Println(r)
+	log.WriteFile("Tempo de execução: ")
+	log.WriteFile(fmt.Sprintln(endTime.Sub(startTime)))
+	fmt.Println("Done")
 }
 
 func updateJson(data any, path []string, teste map[string]string) {
@@ -142,11 +161,12 @@ func updateJson(data any, path []string, teste map[string]string) {
 		for key, val := range v {
 			path = append(path, key)
 			if value, ok := val.(string); ok {
+				log.WriteFile(strings.Join(path, "."))
+
 				teste[strings.Join(path, ".")] = value
 			} else {
 				updateJson(val, path, teste)
 			}
-
 			path = path[:len(path)-1]
 		}
 	}
